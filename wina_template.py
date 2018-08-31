f63df0f2e49f747b9a5819679dc104ed95ba322b
Jannes Höke <jhoeke@uni-bremen.de>
2018-08-31 16:22:07 +0200

    :bento: Add missing WINA template

---
+++ wina_template.py
@@ -0,0 +1,64 @@
+WINA_TEMPLATE = '''<?xml version="1.0" encoding="ISO-8859-1" standalone="no" ?>
+<!DOCTYPE OPENMEDIA SYSTEM "ann_objects.dtd"><OPENMEDIA><OM_SERVER>WINA</OM_SERVER>
+<OM_OBJECT SystemID = "1fc8e208-b05a-4e83-a6b9-1ada17fcb0b5" ObjectID = "0000000100027073" DocumentURN = "urn:openmedia:1fc8e208-b05a-4e83-a6b9-1ada17fcb0b5:0000000100027073" DirectoryID = "0000000100006d66" InternalType = "1" TemplateID = "fffffffa00003bb5" TemplateType = "2" TemplateName = "WINA Meldung">
+<OM_HEADER><OM_FIELD FieldID = "1" FieldType = "3" FieldName = "Erstellt" IsEmpty = "no"><OM_DATETIME>20180827T130446,000</OM_DATETIME></OM_FIELD>
+<OM_FIELD FieldID = "2" FieldType = "3" FieldName = "Stand" IsEmpty = "no"><OM_DATETIME>{sent}</OM_DATETIME></OM_FIELD>
+<OM_FIELD FieldID = "3" FieldType = "1" FieldName = "OwnerName" IsEmpty = "no"><OM_STRING>Bot, Unwetter</OM_STRING></OM_FIELD>
+<OM_FIELD FieldID = "5" FieldType = "1" FieldName = "Autor" IsEmpty = "no"><OM_STRING>Bot, Unwetter</OM_STRING></OM_FIELD>
+<OM_FIELD FieldID = "7" FieldType = "1" FieldName = "CompletionStatus" IsEmpty = "yes"><OM_STRING></OM_STRING></OM_FIELD>
+<OM_FIELD FieldID = "8" FieldType = "1" FieldName = "Titel" IsEmpty = "no"><OM_STRING>{title}</OM_STRING></OM_FIELD>
+<OM_FIELD FieldID = "9" FieldType = "1" FieldName = "Letzte Worte" IsEmpty = "no"><OM_STRING>Testmeldung bitte ignorieren. Hier steht der Text.</OM_STRING></OM_FIELD>
+<OM_FIELD FieldID = "10" FieldType = "1" FieldName = "First Words" IsEmpty = "no"><OM_STRING>Testmeldung bitte ignorieren. Hier steht...</OM_STRING></OM_FIELD>
+<OM_FIELD FieldID = "11" FieldType = "1" FieldName = "Stichworte" IsEmpty = "no"><OM_STRING>Wettermeldung</OM_STRING></OM_FIELD>
+<OM_FIELD FieldID = "13" FieldType = "1" FieldName = "Anchor" IsEmpty = "no"><OM_STRING>Bot, Unwetter</OM_STRING></OM_FIELD>
+<OM_FIELD FieldID = "14" FieldType = "1" FieldName = "Klartext" IsEmpty = "no"><OM_STRING>{text}</OM_STRING></OM_FIELD>
+<OM_FIELD FieldID = "15" FieldType = "1" FieldName = "FormattedText" IsEmpty = "no"><OM_STRING></OM_STRING></OM_FIELD>
+<OM_FIELD FieldID = "16" FieldType = "1" FieldName = "Kategorie" IsEmpty = "no"><OM_STRING>TESTHINWEIS</OM_STRING></OM_FIELD>
+<OM_FIELD FieldID = "20" FieldType = "2" FieldName = "Object type" IsEmpty = "no"><OM_INT32>7</OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "22" FieldType = "2" FieldName = "UpdateUser" IsEmpty = "no"><OM_INT32>18408</OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "28" FieldType = "3" FieldName = "Last time modifed" IsEmpty = "yes"><OM_DATETIME></OM_DATETIME></OM_FIELD>
+<OM_FIELD FieldID = "59" FieldType = "2" FieldName = "Prioritätsnummer" IsEmpty = "no"><OM_INT32>3</OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "60" FieldType = "2" FieldName = "Teleprompter" IsEmpty = "yes"><OM_INT32></OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "61" FieldType = "2" FieldName = "OnAirTrigger" IsEmpty = "yes"><OM_INT32></OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "62" FieldType = "2" FieldName = "OnAirStatus" IsEmpty = "yes"><OM_INT32></OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "84" FieldType = "2" FieldName = "AutomationStatus" IsEmpty = "yes"><OM_INT32></OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "86" FieldType = "1" FieldName = "Agentur" IsEmpty = "no"><OM_STRING>WINA</OM_STRING></OM_FIELD>
+<OM_FIELD FieldID = "100" FieldType = "2" FieldName = "Erstellt von" IsEmpty = "no"><OM_INT32>18408</OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "101" FieldType = "2" FieldName = "Break" IsEmpty = "yes"><OM_INT32></OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "128" FieldType = "3" FieldName = "AiredTime" IsEmpty = "yes"><OM_DATETIME></OM_DATETIME></OM_FIELD>
+<OM_FIELD FieldID = "129" FieldType = "4" FieldName = "AiredDuration" IsEmpty = "yes"><OM_TIMESPAN></OM_TIMESPAN></OM_FIELD>
+<OM_FIELD FieldID = "184" FieldType = "3" FieldName = "ChangedDate" IsEmpty = "no"><OM_DATETIME>{sent}</OM_DATETIME></OM_FIELD>
+<OM_FIELD FieldID = "185" FieldType = "1" FieldName = "Geändert von" IsEmpty = "no"><OM_STRING>Bot, Unwetter</OM_STRING></OM_FIELD>
+<OM_FIELD FieldID = "186" FieldType = "2" FieldName = "ChangedVersion" IsEmpty = "no"><OM_INT32>5</OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "191" FieldType = "2" FieldName = "IconIndex" IsEmpty = "yes"><OM_INT32></OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "205" FieldType = "3" FieldName = "Deletion Date" IsEmpty = "yes"><OM_DATETIME></OM_DATETIME></OM_FIELD>
+<OM_FIELD FieldID = "206" FieldType = "2" FieldName = "Deleted User" IsEmpty = "yes"><OM_INT32></OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "218" FieldType = "2" FieldName = "CreatorID" IsEmpty = "no"><OM_INT32>18408</OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "219" FieldType = "2" FieldName = "Character Count" IsEmpty = "no"><OM_INT32>50</OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "1000" FieldType = "3" FieldName = "Sch. Start Time" IsEmpty = "yes"><OM_DATETIME></OM_DATETIME></OM_FIELD>
+<OM_FIELD FieldID = "1001" FieldType = "3" FieldName = "Sch. End Time" IsEmpty = "yes"><OM_DATETIME></OM_DATETIME></OM_FIELD>
+<OM_FIELD FieldID = "1002" FieldType = "4" FieldName = "Sch. Duration" IsEmpty = "yes"><OM_TIMESPAN></OM_TIMESPAN></OM_FIELD>
+<OM_FIELD FieldID = "1003" FieldType = "3" FieldName = "Pre End" IsEmpty = "yes"><OM_DATETIME></OM_DATETIME></OM_FIELD>
+<OM_FIELD FieldID = "1004" FieldType = "3" FieldName = "Start Time" IsEmpty = "yes"><OM_DATETIME></OM_DATETIME></OM_FIELD>
+<OM_FIELD FieldID = "1005" FieldType = "4" FieldName = "Duration" IsEmpty = "no"><OM_TIMESPAN>0</OM_TIMESPAN></OM_FIELD>
+<OM_FIELD FieldID = "1006" FieldType = "4" FieldName = "Over-Under-Time" IsEmpty = "yes"><OM_TIMESPAN></OM_TIMESPAN></OM_FIELD>
+<OM_FIELD FieldID = "1009" FieldType = "4" FieldName = "Used Duration" IsEmpty = "no"><OM_TIMESPAN>0</OM_TIMESPAN></OM_FIELD>
+<OM_FIELD FieldID = "1010" FieldType = "4" FieldName = "Estimated Duration" IsEmpty = "no"><OM_TIMESPAN>0</OM_TIMESPAN></OM_FIELD>
+<OM_FIELD FieldID = "1011" FieldType = "2" FieldName = "Wortzahl" IsEmpty = "no"><OM_INT32>7</OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "1015" FieldType = "2" FieldName = "Abgenommen" IsEmpty = "no"><OM_INT32>0</OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "1017" FieldType = "2" FieldName = "Finished" IsEmpty = "yes"><OM_INT32></OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "1026" FieldType = "4" FieldName = "TimeCodeDuration" IsEmpty = "no"><OM_TIMESPAN>0</OM_TIMESPAN></OM_FIELD>
+<OM_FIELD FieldID = "1027" FieldType = "2" FieldName = "SpeakerID" IsEmpty = "no"><OM_INT32>18408</OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "1028" FieldType = "4" FieldName = "CorrectionDuration" IsEmpty = "yes"><OM_TIMESPAN></OM_TIMESPAN></OM_FIELD>
+<OM_FIELD FieldID = "1029" FieldType = "4" FieldName = "CorrectionOffset" IsEmpty = "yes"><OM_TIMESPAN></OM_TIMESPAN></OM_FIELD>
+<OM_FIELD FieldID = "1032" FieldType = "4" FieldName = "CorrectionOffset2" IsEmpty = "yes"><OM_TIMESPAN></OM_TIMESPAN></OM_FIELD>
+<OM_FIELD FieldID = "1033" FieldType = "2" FieldName = "TextTiming" IsEmpty = "yes"><OM_INT32></OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "1034" FieldType = "2" FieldName = "InsertTiming" IsEmpty = "yes"><OM_INT32></OM_INT32></OM_FIELD>
+<OM_FIELD FieldID = "1035" FieldType = "4" FieldName = "StoryTextDuration" IsEmpty = "no"><OM_TIMESPAN>3000</OM_TIMESPAN></OM_FIELD>
+<OM_FIELD FieldID = "1036" FieldType = "4" FieldName = "StoryInsertDuration" IsEmpty = "no"><OM_TIMESPAN>0</OM_TIMESPAN></OM_FIELD>
+
+</OM_HEADER>
+
+
+</OM_OBJECT>
+</OPENMEDIA>'''
