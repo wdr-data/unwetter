2031760e83f722df782eb8a1077df833c0e3a125
Timo Cornelius Metzger <>
2018-09-20 12:38:36 +0200

    :recycle: Get channel from os.environ

--- unwetter/slack.py
+++ unwetter/slack.py
@@ -7,23 +7,24 @@ from slackclient import SlackClient
 
 # Set up Slack client (based on https://www.fullstackpython.com/blog/build-first-slack-bot-python.html)
 slack_token = os.environ.get('SLACK_BOT_TOKEN')
+channel = os.environ.get('SLACK_CHANNEL')
 slack_client = SlackClient(slack_token)
 
 
-def send_to_slack(message, channel):  # testing channel: CCKQ5BX5G
+def send_to_slack(message):
     """
     Send message to Slack on channel
     """
     if slack_client.rtm_connect(with_team_state=False):  # Read and print the bot's user ID by calling Web API method `auth.test` to check if connected to Slack
         ta_id = slack_client.api_call("auth.test")["user_id"]
-        print(f'Themen-Agent connected and running as User-ID {ta_id}!')
+        print(f'Themen-Agent connected and running as User-ID {ta_id}!')       
         
         slack_client.api_call(
             'chat.postMessage',
             channel=channel,
             text=message
             )
-
+        
         print(f'Message sent to Slack: {message}')
         
     else:
