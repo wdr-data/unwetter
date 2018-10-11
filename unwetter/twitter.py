61b08a959ac460cd9886a42206425d12fca13125
Jannes Höke <jhoeke@uni-bremen.de>
2018-10-11 11:47:18 +0200

    :ambulance: Use correct environment variable

--- unwetter/twitter.py
+++ unwetter/twitter.py
@@ -3,7 +3,8 @@ import os
 import tweepy
 
 
-_auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+_auth = tweepy.OAuthHandler(
+    os.environ['TWITTER_CONSUMER_KEY'], os.environ['TWITTER_CONSUMER_SECRET'])
 _auth.set_access_token(
     os.environ['TWITTER_ACCESS_TOKEN'], os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
 
