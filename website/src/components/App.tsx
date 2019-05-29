f4195c3396e5d7121fd7d6fd5d3eba711ff54c99
Jannes Höke <jhoeke@uni-bremen.de>
2019-05-29 18:44:34 +0200

    :sparkles: Create initial working website prototype

--- website/src/components/App.tsx
+++ website/src/components/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
-import styles from './App.module.scss';
+import React from "react";
+import AppBar from "@material-ui/core/es/AppBar";
+import Button from "@material-ui/core/es/Button";
+import Toolbar from "@material-ui/core/es/Toolbar";
+import Typography from "@material-ui/core/es/Typography";
+import { Router, RouteComponentProps } from "@reach/router";
+
+import styles from "./App.module.scss";
+import Configurator from "./maps/Configurator";
 
 const App: React.FC = () => {
-  return <div />;
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className={styles.white} variant="h6">
+            UWA Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Router>
+        <Configurator path="/events/*" />
+      </Router>
+    </>
+  );
 };
 
 export default App;
---
+++ website/src/components/maps/Configurator.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useCallback } from "react";
+import Button from "@material-ui/core/es/Button";
+import Typography from "@material-ui/core/es/Typography";
+import Paper from "@material-ui/core/es/Paper";
+import TextField from "@material-ui/core/es/TextField";
+import Grid from "@material-ui/core/es/Grid";
+import classNames from "classnames";
+import moment from "moment";
+
+import { RouteComponentProps } from "@reach/router";
+
+import styles from "./Configurator.module.scss";
+import { useFormField } from "../hooks/form";
+import { async } from "q";
+
+const Configurator: React.FC<RouteComponentProps> = () => {
+  const [date, changeDateHandler] = useFormField("");
+  const [time, changeTimeHandler] = useFormField("");
+
+  const [events, setEvents] = useState([]);
+
+  const refreshMap = useCallback(async () => {
+    const m = moment(`${date}T${time}`, "");
+    const timestamp = m.format("X");
+    setEvents(
+      await (await fetch(`/api/v1/events/current?at=${timestamp}`)).json()
+    );
+  }, [date, time]);
+
+  return (
+    <div className={styles.configurator}>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Paper className={styles.paper}>
+            <TextField
+              label="Datum"
+              margin="normal"
+              type="date"
+              InputLabelProps={{
+                shrink: true
+              }}
+              onChange={changeDateHandler}
+            />
+            <TextField
+              label="Zeit"
+              margin="normal"
+              type="time"
+              InputLabelProps={{
+                shrink: true
+              }}
+              onChange={changeTimeHandler}
+            />
+            <br />
+            <TextField label="Titel" margin="normal" /> <br />
+            <TextField label="Untertitel" margin="normal" /> <br />
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={styles.paper}>
+            <img
+              className={classNames(styles.image, styles.marginBottom)}
+              src={`/map?id=${events.map((ev: any) => ev.id).join("&id=")}`}
+              alt="Map of the event"
+            />
+            <Button color="secondary" variant="contained" onClick={refreshMap}>
+              Refresh
+            </Button>
+            <Button color="primary" variant="contained">
+              Download
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={styles.paper}>foo</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Configurator;
