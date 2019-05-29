a8f1defbd5730d4acfb53f44097a001687f725c6
Jannes Höke <jhoeke@uni-bremen.de>
2019-05-29 13:07:43 +0200

    :sparkles: Generate maps using premade images

--- README.md
+++ README.md
@@ -1,32 +1,46 @@
 # Unwetter
+
 Automatically notify newsroom about weather warnings from DWD
+
+## Map generation
+
+The map design and source images are proprietary. To make your own design, do the following steps:
+
+1. Start a local development Flask instance using `FLASK_APP=app FLASK_ENV=development pipenv run flask run`
+2. Navigate to http://127.0.0.1:5000/basemap to get a black/white map template
+3. Use the template to create your own map design
+4. Replace the files in `assets/images`
 
 ## Deployment
 
 ### Environment variables
+
 ```yaml
-NVS_FTP_URL:                 # FTP host for Nachrichtenverteilsystem uploads
+NVS_FTP_URL: # FTP host for Nachrichtenverteilsystem uploads
 NVS_FTP_PASS:
 NVS_FTP_USER:
-SENTRY_DSN:                  # DSN for sentry.io for error tracking
-SLACK_BOT_TOKEN:             # Bot token for sending messages to Slack
-SLACK_CHANNEL:               # Channel ID to send updates to
-TWITTER_ACCESS_TOKEN: 
+SENTRY_DSN: # DSN for sentry.io for error tracking
+SLACK_BOT_TOKEN: # Bot token for sending messages to Slack
+SLACK_CHANNEL: # Channel ID to send updates to
+TWITTER_ACCESS_TOKEN:
 TWITTER_ACCESS_TOKEN_SECRET:
 TWITTER_CONSUMER_KEY:
 TWITTER_CONSUMER_SECRET:
-TWITTER_DEMO_HANDLE:         # Twitter handle that should be mentioned in demo tweets
-WDR_PROJECT_INFO_URL:        https://unser.wdr.de/sites/DWDUnwetterwarnungen94e47adr/SitePages/Details%20Unwetterwarnungen.aspx
+TWITTER_DEMO_HANDLE: # Twitter handle that should be mentioned in demo tweets
+WDR_PROJECT_INFO_URL: https://unser.wdr.de/sites/DWDUnwetterwarnungen94e47adr/SitePages/Details%20Unwetterwarnungen.aspx
 ```
 
 ### Heroku Labs
+
 - Runtime dyno metadata for linking releases to sentry
-    - `heroku labs:enable runtime-dyno-metadata -a <appname>`
+  - `heroku labs:enable runtime-dyno-metadata -a <appname>`
 
 ### Scheduled Jobs
-After deployment, run `heroku ps:scale clock=1` to make sure that no two schedulers are running 
+
+After deployment, run `heroku ps:scale clock=1` to make sure that no two schedulers are running
 at the same time.
 
 ### Heroku release tracking in Sentry
-Follow [this guide](https://blog.sentry.io/2017/05/15/heroku-commits) to connect Heroku and Sentry 
+
+Follow [this guide](https://blog.sentry.io/2017/05/15/heroku-commits) to connect Heroku and Sentry
 for better release tracking.
--- src/unwetter/map.py
+++ src/unwetter/map.py
@@ -10,30 +10,25 @@ from .data.shapes import STATE_SHAPES
 from .config import STATES_FILTER
 
 
-with open('config/studios.yml', 'r') as fp:
-    STUDIOS = yaml.safe_load(fp.read())
+TARGET_HEIGHT = 1080
 
 
 COLORS = {
-    'BACKGROUND': 'rgb(242, 238, 226)',
-    'STATE': '#81db51',
-    'SURROUNDINGS': 'rgb(242, 238, 226)',
-    'BORDERS': 'white',
-    'STUDIOS': 'rgba(44, 44, 44, 77)',
     'SEVERITIES': {
-        'Minor': '#ffcc00',
-        'Moderate': '#ff6600',
-        'Severe': '#ff0000',
-        'Extreme': '#cc3399',
+        'Minor': '#ffcc00b0',
+        'Moderate': '#ff6600b0',
+        'Severe': '#ff0000b0',
+        'Extreme': '#cc3399b0',
     }
 }
 
+ERROR_FONT = ImageFont.truetype('assets/fonts/VT323-Regular.ttf', 300)
 
-target_projection = pyproj.Proj(init='epsg:3857')
+TARGET_PROJECTION = pyproj.Proj(init='epsg:3857')
 project = partial(
     pyproj.transform,
     pyproj.Proj(init='epsg:4326'),  # source coordinate system
-    target_projection
+    TARGET_PROJECTION
 )
 
 
@@ -66,8 +61,8 @@ bbox = (
 
 dist_x = bbox[2] - bbox[0]
 dist_y = bbox[3] - bbox[1]
-img_width_ = int(dist_x / 150)
-img_height_ = int(dist_y / 150)
+img_width_ = int(dist_x / 250)
+img_height_ = int(dist_y / 250)
 img_height = max(img_height_, img_width_)
 img_width = max(img_height_, img_width_)
 ratio_x = img_width_ / dist_x
@@ -80,98 +75,76 @@ def to_image_coords(x, y):
     return px, py
 
 
-def draw_background():
-    img = Image.new("RGBA", (img_width, img_height), COLORS['BACKGROUND'])
+def resize(img):
+    return img.resize((TARGET_HEIGHT, TARGET_HEIGHT), resample=Image.CUBIC)
+
+
+background_image = resize(Image.open('assets/images/bg_square.png').convert("RGBA"))
+overlay = resize(Image.open('assets/images/overlay_square.png').convert("RGBA"))
+
+
+def generate_base_map():
+    img = Image.new("RGBA", (img_width, img_height), 'black')
     draw = ImageDraw.Draw(img)
 
     for poly in states:
         points = [to_image_coords(x, y) for x, y in poly.exterior.coords]
-        draw.polygon(points, outline=None, fill=COLORS['STATE'])
+        draw.polygon(points, outline=None, fill='white')
 
         for interior in poly.interiors:
             points = [to_image_coords(x, y) for x, y in interior.coords]
-            draw.polygon(points, outline=None, fill=COLORS['BACKGROUND'])
+            draw.polygon(points, outline=None, fill='black')
 
     return img
 
-
-def draw_surroundings(img):
-    draw = ImageDraw.Draw(img)
-
-    for poly in surroundings:
-        points = [to_image_coords(x, y) for x, y in poly.exterior.coords]
-        draw.polygon(points, outline=None, fill=COLORS['SURROUNDINGS'])
-
-
-def draw_studios(img):
-    draw = ImageDraw.Draw(img, 'RGBA')
-    size = 15
-
-    for name, studio in STUDIOS.items():
-        coords = studio['coordinates']
-        point = to_image_coords(*target_projection(coords['lat'], coords['lon']))
-        draw.rectangle(
-            [(point[0] - size, point[1] - size),
-             (point[0] + size, point[1] + size)],
-            fill=COLORS['STUDIOS'])
-
-
-def draw_borders(img, width=3):
-    draw = ImageDraw.Draw(img)
-
-    line_width = width
-    point_radius = int(line_width / 3)
-
-    for poly in states.geoms:
-        for line in list(poly.interiors) + [poly.exterior]:
-            points = [to_image_coords(*point) for point in line.coords]
-
-            draw.line(points, fill=COLORS['BORDERS'], width=line_width)
-            for point in points:
-                draw.ellipse(
-                    (point[0] - point_radius,
-                     point[1] - point_radius,
-                     point[0] + point_radius,
-                     point[1] + point_radius),
-                    fill=COLORS['BORDERS'])
-
-
-
-def draw_overlay():
-    img = Image.new("RGBA", (img_width, img_height))
-
-    draw_surroundings(img)
-    # draw_borders(img)
-    draw_studios(img)
-
-    return img
-
-
-def draw_event(event):
-    img = background_image.copy()
-    draw = ImageDraw.Draw(img)
-
+def draw_event(event, draw):
     if event:
         for geo in event['geometry']:
-
             for poly in geo['polygons']:
-                projected = [to_image_coords(*target_projection(lng, lat)) for lat, lng in poly]
+                projected = [to_image_coords(*TARGET_PROJECTION(lng, lat)) for lat, lng in poly]
                 draw.polygon(projected, outline=None, fill=COLORS['SEVERITIES'][event['severity']])
 
             for poly in geo['exclude_polygons']:
-                projected = [to_image_coords(*target_projection(lng, lat)) for lat, lng in poly]
-                draw.polygon(projected, outline=None, fill=COLORS['STATE'])
+                projected = [to_image_coords(*TARGET_PROJECTION(lng, lat)) for lat, lng in poly]
+                draw.polygon(projected, outline=None, fill='rgba(0, 0, 0, 0)')
 
-            img.alpha_composite(overlay)
     else:
-        img.alpha_composite(overlay)
-        fnt = ImageFont.truetype('assets/fonts/VT323-Regular.ttf', 300)
-        draw.text((200, img_height / 2 - 150), "Event not found", font=fnt, fill=(0, 0, 0, 255))
-
-    resized = img #.resize((int(img_width * .5), int(img_height * .5)), resample=Image.CUBIC)
-
-    return resized
+        draw.text((200, img_height / 2 - 150), "Event not found", font=ERROR_FONT, fill='black')
 
 
-background_image = draw_background()
-overlay = draw_overlay()
+def severity_key(event):
+
+    mapped = {
+        'Minor': 0,
+        'Moderate': 1,
+        'Severe': 2,
+        'Extreme': 3,
+    }
+
+    return mapped.get(event['severity'], 100)
+
+
+def generate_map(events):
+    from datetime import datetime
+    start = datetime.now()
+
+    img = background_image.copy()
+    print('bg', datetime.now() - start)
+
+
+    event_img = Image.new("RGBA", (img_width, img_height))
+    print('created event image', datetime.now() - start)
+    draw = ImageDraw.Draw(event_img)
+    print('created event draw', datetime.now() - start)
+
+    for event in sorted(events, key=severity_key):
+        draw_event(event, draw)
+        print('drew event', datetime.now() - start)
+
+    img.alpha_composite(resize(event_img))
+    print('first composite/resize', datetime.now() - start)
+
+    img.alpha_composite(overlay)
+    print('second composite', datetime.now() - start)
+
+    return img
