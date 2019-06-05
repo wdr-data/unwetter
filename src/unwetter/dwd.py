c9e5f832c23235c6624cbd9173063377332292ec
Jannes Höke <mail@jhoeke.de>
2019-06-05 12:17:30 +0200

    :bug: Remove tz_info from decoded events

--- src/unwetter/dwd.py
+++ src/unwetter/dwd.py
@@ -90,7 +90,7 @@ def parse_xml(xml):
     for field_xml, field_json in fields.items():
         event[field_json] = xml_dict.get(field_xml)
         if field_json in datetime_fields and event[field_json]:
-            event[field_json] = iso8601.parse_date(event[field_json]).astimezone(pytz.utc)
+            event[field_json] = iso8601.parse_date(event[field_json]).astimezone(pytz.utc).replace(tzinfo=None)
 
     info_fields = {
         'event': 'event',
@@ -111,7 +111,7 @@ def parse_xml(xml):
     for field_xml, field_json in info_fields.items():
         event[field_json] = xml_dict['info'].get(field_xml)
         if field_json in datetime_fields and event[field_json]:
-            event[field_json] = iso8601.parse_date(event[field_json]).astimezone(pytz.utc)
+            event[field_json] = iso8601.parse_date(event[field_json]).astimezone(pytz.utc).replace(tzinfo=None)
 
     for tag in ('area', 'parameter'):
         try:
