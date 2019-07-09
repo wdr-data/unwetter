fe53c26998f5f12e58ef9ae5de43cebff44242bd
ChristianJoe <cjoe@gmx.de>
2019-07-09 17:00:42 +0200

    :bug: Place `.capitalize()` inside of f-string variable

--- src/unwetter/generate/blocks.py
+++ src/unwetter/generate/blocks.py
@@ -132,9 +132,7 @@ def title(event, variant='default'):
     if variant == 'default':
         return f'{extention}: {event["headline"]}'
     elif variant == 'wina_body':
-        return f'{extention}.capitalize()' \
-               f'' \
-               f'{event["headline"]}'
+        return f'{extention.capitalize()}\n\n{event["headline"]}'
     elif variant == 'wina_headline':
         return f'Amtliche Unwetterwarnung des DWD (UWA) {extention}'
 
