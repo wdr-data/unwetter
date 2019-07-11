04e81ad23ad13742728b100d75e71ba3f46cfed1
ChristianJoe <cjoe@gmx.de>
2019-07-11 19:26:57 +0200

    :crossing_children: Point out not how to use text suggestions

--- src/unwetter/generate/__init__.py
+++ src/unwetter/generate/__init__.py
@@ -134,6 +134,9 @@ Verhaltenshinweise: {event['instruction'] or ''}
 
 
 +++ Textvorschläge +++
+HINWEIS: Textvorschläge werden nach redaktionellen Vorgaben automatisch generiert.
+Je nach Unwetterlage ist es nötig, sie noch einmal passgenau zu überarbeiten
+und dabei auch die eventuellen Warnungen vor verschiedenen Unwettergebieten zusammenzufassen.
 
 TWEET: {tweet(event)}
 
