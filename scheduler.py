35fc91fbcb03a3e591fbd5b151acce5174ceedc4
ChristianJoe <cjoe@gmx.de>
2019-07-11 19:35:15 +0200

    :crossing_children: Point out clear warning in wina headline

--- scheduler.py
+++ scheduler.py
@@ -72,14 +72,14 @@ def post_clear_warning():
     elif db.warn_events_memo() and not currently_events:
         db.set_warn_events_memo(False)
 
-        title = 'Warnfplicht für NRW aufgehoben'
         text = '''
 AKTUALISIERUNG:
 Der Deutsche Wetterdienst gibt für NRW zurzeit keine Warnungen der Kategorie 3 (rot) und 4 (violett) mehr aus -
 also vor Unwetter oder extremem Unwetter. Damit besteht keine Warnpflicht mehr. Es kann allerdings nach wie vor
 markante Wetterlagen geben - alle Informationen dazu auf einen Blick hier:
 
-UWA-Kartentool: www.wdr.de/k/unwetterkarte
+UWA-Karten: www.wdr.de/k/unwetterkarte
+(Keine roten und violetten Unwettergebiete)
 
 https://www.dwd.de/DE/wetter/warnungen/warnWetter_node.html
 (Vgl. NRW auf Website des Deutschen Wetterdienstes)
@@ -87,9 +87,11 @@ https://www.dwd.de/DE/wetter/warnungen/warnWetter_node.html
 
         keywords = 'Unwetter, UWA, Keine Warnpflicht'
 
-        wina.upload_text(title, text, keywords)
+        title_wina = 'Amtliche Unwetterwarnung des DWD (UWA) - Keine Warnpflicht'
+        wina.upload_text(title_wina, text, keywords)
 
-        slack.post_text(title, text)
+        title_slack = 'Warnfplicht für NRW aufgehoben'
+        slack.post_text(title_slack, text)
 
         print('No active events: warn_events_memo OFF')
 
